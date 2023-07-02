4ebd64457369208b95a6bae7d37a68ba0f644911
PK401 <prafulkapse108@gmail.com>
2023-07-02 15:24:16 +0530

    this is my commit for ignore files

---
+++ ajay.java
@@ -0,0 +1,6 @@
+tThis is new java file we  hich is r created after dii ett  eleting fiel   le via rese  vert
+thisis  is new ava    [3~[3~java program
+
+add this code pt ron    test branch
+adding this code from master branch
+
---
+++ praful.py
@@ -0,0 +1,2 @@
+thi s is my                I am praful i am using this file to ca reate thse file pyton   thi[C[C on
+
